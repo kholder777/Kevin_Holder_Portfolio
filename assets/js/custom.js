6f11cf44975582c23678f5e284a3f8856ddf256d
Kevin Holder <kholder777@yahoo.com>
2020-04-24 11:31:01 -0500

    added assets folder. Modified all links to accommodate said change.

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+// $grid - columns: 12;
+// $grid - gutter - width: 30 px;
+
+// $grid - breakpoints: (
+//     // Extra small screen / phone
+//     xs: 0,
+//     // Small screen / phone
+//     sm: 576 px,
+//     // Medium screen / tablet
+//     md: 768 px,
+//     // Large screen / desktop
+//     lg: 992 px,
+//     // Extra large screen / wide desktop
+//     xl: 1200 px
+// );
+
+// $container - max - widths: (
+//     sm: 540 px,
+//     md: 720 px,
+//     lg: 960 px,
+//     xl: 1140 px
+// );
+// $grid - breakpoints: (
+//     xs: 0,
+//     sm: 480 px,
+//     md: 768 px,
+//     lg: 1024 px
+// );
+
+// $container - max - widths: (
+//     sm: 420 px,
+//     md: 720 px,
+//     lg: 960 px
+// );
